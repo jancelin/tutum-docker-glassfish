8a649e10389eaaa35580524be1ff328e10ed41de
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2015-07-15 14:37:45 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -7,9 +7,10 @@ set -e
 asadmin start-domain 
 if [ ! -f /.glassfish_admin_password_changed ]; then
     /change_admin_password.sh
+    asadmin --user=admin create-jvm-options "-Duser.timezone=Europe/Paris"
 fi
 echo "=> Restarting Glassfish server"
 asadmin stop-domain
 echo "=> Starting and running Glassfish server"
 asadmin start-domain -w
-asadmin --user=admin create-jvm-options "-Duser.timezone=Europe/Paris"
+
