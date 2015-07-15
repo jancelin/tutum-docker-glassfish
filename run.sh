3d839e9ad35557f82dab68a55301f7a24bd0293f
ANCELIN Julien <julien.ancelin@stlaurent.lusignan.inra.fr>
2015-07-15 14:58:04 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,11 @@
 set -e
 
 asadmin start-domain 
+# timezone paris
 asadmin --user=admin create-jvm-options "-Duser.timezone=Europe/Paris"
+# changement de port
+asadmin set server.http-service.http-listener.http-listener-1.port=80
+
 if [ ! -f /.glassfish_admin_password_changed ]; then
     /change_admin_password.sh
     
